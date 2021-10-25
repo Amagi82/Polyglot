b79be3c66c52aaa60a29af256a46f077cc70e3fd
Jim <jim@brilliant.org>
2021-10-25 13:22:21 -0700

    use built-in enum ordinal

--- src/main/kotlin/project/ResourceType.kt
+++ src/main/kotlin/project/ResourceType.kt
@@ -4,12 +4,5 @@ import androidx.compose.runtime.Immutable
 
 @Immutable
 enum class ResourceType {
-    STRINGS, PLURALS, ARRAYS;
-
-    val index: Int
-        get() = when (this) {
-            STRINGS -> 0
-            PLURALS -> 1
-            ARRAYS -> 2
-        }
+    STRINGS, PLURALS, ARRAYS
 }
