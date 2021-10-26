d3ffb595c1651b82fe7d6b7c17cb2e23efc6d533
Jim <Amagi82@gmail.com>
2021-10-26 01:30:27 -0700

    OutlinedTextFields -> DenseTextFields

--- src/main/kotlin/ui/resource/ResourceRow.kt
+++ src/main/kotlin/ui/resource/ResourceRow.kt
@@ -1,49 +1,50 @@
 package ui.resource
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.map
-import locales.Locale
 import locales.LocaleIsoCode
 import project.*
+import ui.core.DenseTextField
 import ui.core.IconButton
-import ui.core.onPressEnter
+import ui.core.focusNextOnEnter
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun <R : Resource> ResourceRow(vm: ResourceViewModel, resourceVM: ResourceTypeViewModel<R>, resId: ResourceId) {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        val focusManager = LocalFocusManager.current
         Column(modifier = Modifier.weight(1f)) {
             var id by remember { mutableStateOf(resId) }
             var error by remember { mutableStateOf("") }
 
-            OutlinedTextField(
+            DenseTextField(
                 value = id.value,
                 onValueChange = {
                     error = ""
                     id = ResourceId(it.dropWhile(Char::isDigit).filter(Char::isLetterOrDigit))
                 },
-                modifier = Modifier.padding(vertical = 4.dp).onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }.onFocusChanged {
-                    if (!it.hasFocus && resId != id) {
-                        if (id.value.isEmpty()) {
-                            resourceVM.removeResource(resId)
-                        } else if (!resourceVM.updateResourceId(resId, id)) {
-                            error = "id already exists"
+                modifier = Modifier.padding(vertical = 4.dp).fillMaxWidth()
+                    .focusNextOnEnter()
+                    .onFocusChanged {
+                        if (!it.hasFocus && resId != id) {
+                            if (id.value.isEmpty()) {
+                                resourceVM.removeResource(resId)
+                            } else if (!resourceVM.updateResourceId(resId, id)) {
+                                error = "id already exists"
+                            }
                         }
-                    }
-                },
-                label = { Text("id") },
+                    },
                 isError = error.isNotEmpty(),
-                singleLine = true
+                singleLine = true,
             )
             if (error.isNotEmpty()) {
                 Text(error, color = MaterialTheme.colors.error)
@@ -53,10 +54,10 @@ fun <R : Resource> ResourceRow(vm: ResourceViewModel, resourceVM: ResourceTypeVi
         if (resourceVM is ArrayResourceViewModel) {
             val oldSize by resourceVM.arraySize(resId).collectAsState(1)
             var newSize by remember(oldSize) { mutableStateOf(oldSize) }
-            OutlinedTextField(
+            DenseTextField(
                 value = newSize.toString(),
                 onValueChange = { newSize = it.filter(Char::isDigit).toIntOrNull()?.coerceAtLeast(1) ?: 1 },
-                modifier = Modifier.padding(start = 8.dp).width(72.dp).onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }.onFocusChanged {
+                modifier = Modifier.padding(start = 8.dp).width(64.dp).focusNextOnEnter().onFocusChanged {
                     if (!it.hasFocus && oldSize != newSize) {
                         resourceVM.updateArraySize(resId, newSize)
                     }
@@ -89,46 +90,38 @@ fun <R : Resource> ResourceRow(vm: ResourceViewModel, resourceVM: ResourceTypeVi
 
 @Composable
 fun RowScope.StringField(vm: StringResourceViewModel, resId: ResourceId, localeIsoCode: LocaleIsoCode) {
-    val isDefaultLocale by vm.project.map { it.defaultLocale == localeIsoCode }.collectAsState(false)
     val resource by vm.resource(resId, localeIsoCode).map { it ?: Str() }.collectAsState(Str())
-    val focusManager = LocalFocusManager.current
     var oldText = remember(resource) { resource.text }
     var newText by remember(resource) { mutableStateOf(oldText) }
-    OutlinedTextField(
+    DenseTextField(
         value = newText,
         onValueChange = { newText = it },
-        modifier = Modifier.weight(1f).onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }.onFocusChanged {
+        modifier = Modifier.weight(1f).focusNextOnEnter().onFocusChanged {
             if (!it.hasFocus && oldText != newText) {
                 oldText = newText
                 vm.updateResource(localeIsoCode, resId, Str(newText))
             }
         },
-        label = { Text(Locale[localeIsoCode].displayName(isDefaultLocale)) },
         singleLine = true
     )
 }
 
 @Composable
 fun RowScope.PluralFields(vm: PluralResourceViewModel, resId: ResourceId, localeIsoCode: LocaleIsoCode) {
-    Column(modifier = Modifier.weight(1f)) {
-        val isDefaultLocale by vm.project.map { it.defaultLocale == localeIsoCode }.collectAsState(false)
+    Column(modifier = Modifier.weight(1f).padding(vertical = 2.dp)) {
         val resource by vm.resource(resId, localeIsoCode).map { it ?: Plural() }.collectAsState(Plural())
-        val focusManager = LocalFocusManager.current
-
-        Text(Locale[localeIsoCode].displayName(isDefaultLocale))
         Quantity.values().forEach { quantity ->
             var oldText = remember(resource) { resource[quantity] ?: "" }
             var newText by remember(oldText) { mutableStateOf(oldText) }
-            OutlinedTextField(
+            DenseTextField(
                 value = newText,
                 onValueChange = { newText = it },
-                modifier = Modifier.fillMaxWidth().onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }.onFocusChanged {
+                modifier = Modifier.padding(vertical = 2.dp).fillMaxWidth().focusNextOnEnter().onFocusChanged {
                     if (!it.hasFocus && oldText != newText) {
                         oldText = newText
                         vm.updateResource(localeIsoCode, resId, Plural(resource.items.plus(quantity to newText)))
                     }
                 },
-                label = { Text(quantity.label) },
                 singleLine = true
             )
         }
@@ -138,25 +131,21 @@ fun RowScope.PluralFields(vm: PluralResourceViewModel, resId: ResourceId, locale
 
 @Composable
 fun RowScope.ArrayFields(vm: ArrayResourceViewModel, resId: ResourceId, localeIsoCode: LocaleIsoCode) {
-    val isDefaultLocale by vm.project.map { it.defaultLocale == localeIsoCode }.collectAsState(false)
-    val focusManager = LocalFocusManager.current
-    val size by vm.arraySize(resId).collectAsState(1)
-    val items by vm.resource(resId, localeIsoCode).map { it?.items ?: listOf() }.collectAsState(listOf())
-
-    Column(modifier = Modifier.weight(1f)) {
+    Column(modifier = Modifier.weight(1f).padding(vertical = 2.dp)) {
+        val size by vm.arraySize(resId).collectAsState(1)
+        val items by vm.resource(resId, localeIsoCode).map { it?.items ?: listOf() }.collectAsState(listOf())
         for (index in 0 until size) {
             var oldText = remember(items) { items.getOrElse(index) { "" } }
             var newText by remember(oldText) { mutableStateOf(oldText) }
-            OutlinedTextField(
+            DenseTextField(
                 value = newText,
                 onValueChange = { newText = it },
-                modifier = Modifier.fillMaxWidth().onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }.onFocusChanged {
+                modifier = Modifier.padding(vertical = 2.dp).fillMaxWidth().focusNextOnEnter().onFocusChanged {
                     if (!it.hasFocus && oldText != newText) {
                         oldText = newText
                         vm.updateResource(localeIsoCode, resId, StringArray(List(size) { i -> if (i == index) newText else items.getOrElse(i) { "" } }))
                     }
                 },
-                label = { Text(Locale[localeIsoCode].displayName(isDefaultLocale)) },
                 singleLine = true
             )
         }
