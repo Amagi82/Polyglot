62649786ab978254bbd3d14f6a08c3a3b431adea
Jim <Amagi82@gmail.com>
2021-10-26 01:27:36 -0700

    add convenience function for moving focus to next when enter pressed

--- src/main/kotlin/ui/core/ModifierExtensions.kt
+++ src/main/kotlin/ui/core/ModifierExtensions.kt
@@ -2,10 +2,18 @@ package ui.core
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.focus.*
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.platform.LocalFocusManager
 
 // Return true to stop propagation of this event.
 @OptIn(ExperimentalComposeUiApi::class)
 fun Modifier.onPressEnter(onEvent: () -> Boolean): Modifier = onPreviewKeyEvent {
     it.key == Key.Enter && it.type == KeyEventType.KeyUp && onEvent()
+}
+
+fun Modifier.focusNextOnEnter() = composed {
+    val focusManager = LocalFocusManager.current
+    onPressEnter { focusManager.moveFocus(FocusDirection.Next); true }
 }
