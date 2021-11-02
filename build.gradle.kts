1127d4ac21acfa1f40472378de0f06e6ef4538e7
Jim <Amagi82@gmail.com>
2021-11-01 21:41:33 -0700

    update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0-beta1"
+    id("org.jetbrains.compose") version "1.0.0-beta5"
 }
 
 repositories {
