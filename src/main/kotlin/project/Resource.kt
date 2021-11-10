74e8be93d34d9857cb0d7678b0e12ccfbe73d120
Jim <Amagi82@gmail.com>
2021-11-09 16:58:14 -0800

    get rid of unused constructor

--- src/main/kotlin/project/Resource.kt
+++ src/main/kotlin/project/Resource.kt
@@ -17,22 +17,6 @@ value class Str(val text: String = "") : Resource
 @Immutable
 @JvmInline
 value class Plural(val items: Map<Quantity, String> = mapOf(Quantity.ONE to "", Quantity.OTHER to "")) : Resource {
-    constructor(
-        zero: String? = null,
-        one: String?,
-        two: String? = null,
-        few: String? = null,
-        many: String? = null,
-        other: String
-    ) : this(buildMap {
-        if (zero != null) put(Quantity.ZERO, zero)
-        if (one != null) put(Quantity.ONE, one)
-        if (two != null) put(Quantity.TWO, two)
-        if (few != null) put(Quantity.FEW, few)
-        if (many != null) put(Quantity.MANY, many)
-        put(Quantity.OTHER, other)
-    })
-
     operator fun get(quantity: Quantity) = items[quantity]
 }
 
