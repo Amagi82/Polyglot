40c25057aafdf7f10e6400016a9d8aae363e9af5
Jim <Amagi82@gmail.com>
2021-11-16 15:44:22 -0800

    let PropertyStore manage the File it uses

--- src/main/kotlin/utils/extensions/Resources.kt
+++ src/main/kotlin/utils/extensions/Resources.kt
@@ -2,8 +2,7 @@ package utils.extensions
 
 import R
 import utils.PropertyStore
+import java.io.File
 
 inline fun <reified K, V> loadResource(resource: String, transformer: (Map.Entry<String, String>) -> Pair<K, V>): Map<K, V> =
-    R::class.java.classLoader.getResource(resource)!!.openStream().use {
-        PropertyStore(it).entries.associate(transformer)
-    }
+    File(R::class.java.classLoader.getResource(resource)!!.toURI()).let(::PropertyStore).entries.associate(transformer)
