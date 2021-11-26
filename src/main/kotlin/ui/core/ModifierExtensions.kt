df1cd6e258361bb12c6ef239ecc5431764577b61
Jim <Amagi82@gmail.com>
2021-11-26 01:44:21 -0800

    remove unused imports

--- src/main/kotlin/ui/core/ModifierExtensions.kt
+++ src/main/kotlin/ui/core/ModifierExtensions.kt
@@ -2,10 +2,7 @@ package ui.core
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.focus.*
 import androidx.compose.ui.input.key.*
-import androidx.compose.ui.platform.LocalFocusManager
 
 // Return true to stop propagation of this event.
 @OptIn(ExperimentalComposeUiApi::class)
