2529afe51b1f13a81d031d31690f977425a9596b
Jim <Amagi82@gmail.com>
2021-11-26 01:09:09 -0800

    use MutableStateFlow everywhere and remove unused ReadWriteProperty

--- src/main/kotlin/data/SettingsStore.kt
+++ src/main/kotlin/data/SettingsStore.kt
@@ -1,11 +1,12 @@
 package data
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import project.Project
 import java.io.File
 
 object SettingsStore : PropertyStore(File(Project.projectsFolder, "settings.properties")) {
-    var isDarkTheme: Boolean by prop(getValue = String?::toBoolean)
-    var currentProject: String? by prop(getValue = { it })
+    val isDarkTheme: MutableStateFlow<Boolean> = mutableStateFlowOf("isDarkTheme", getter = String?::toBoolean)
+    val currentProject: MutableStateFlow<String?> = mutableStateFlowOf("currentProject", getter = { it }, setter = { it.orEmpty() })
 
     override fun save() {
         store("Polyglot settings") { println("Failed to save settings with $it") }
